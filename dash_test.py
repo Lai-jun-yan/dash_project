96fa2f13fca9c3253d69cabfc1cf25c674b69f12
unknown <jlai23_ad@wulab.local>
2025-03-14 16:55:09 +0800

    Put item in the table

--- dash_test.py
+++ dash_test.py
@@ -1,5 +1,37 @@
-import subprocess
+import boto3
 
-# 執行 AWS CLI 指令並獲取結果
-result = subprocess.run(["aws", "dynamodb", "list-tables"], capture_output=True, text=True)
-print(result.stdout)
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
+# on the table resource are accessed or its load() method is called.
+table = dynamodb.Table('users')
+
+# Print out some data about the table.
+# This will cause a request to be made to DynamoDB and its attribute
+# values will be set based on the response.
+print(table.creation_date_time)
+
+table.put_item(
+   Item={
+        'username': 'janedoe',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'account_type': 'standard_user',
+    }
+)
+
+table.put_item(
+   Item={
+        'username': '謝瑞廷',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'account_type': 'standard_user',
+    }
+)
+
